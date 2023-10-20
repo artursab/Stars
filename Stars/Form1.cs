946a0d53873d29c6cc561411b4d0032ead8562f4
artursab <arturs.belavskis@gmail.com>
2023-10-20 23:38:10 +0200

    map

--- Stars/Form1.cs
+++ Stars/Form1.cs
@@ -12,9 +12,53 @@ namespace Stars
 {
     public partial class Form1 : Form
     {
+
+        public class Star
+        {
+            public float X {  get; set; }
+            public float Y {  get; set; }
+            public float Z {  get; set; }
+        }
+      private Star[] stars = new Star[15000];
+
+        private Random random = new Random();
+
+        private Graphics graphics;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private float Map(float n, float start1, float stop1, float start2, float stop2)
+        {
+            return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            var result = Map(10, 0 , 100, 0, 1000);
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new Star()
+                {
+                    X = random.Next(-pictureBox1.Width, pictureBox1.Width),
+                    Y = random.Next(-pictureBox1.Height, pictureBox1.Height),
+                    Z = random.Next(1, pictureBox1.Width)
+                };
+            }
+
         }
     }
 }
