54547f1ff51c972a1c3c7ce63b7dce79645961a3
artursab <arturs.belavskis@gmail.com>
2023-10-20 23:53:46 +0200

    move

--- Stars/Form1.cs
+++ Stars/Form1.cs
@@ -32,7 +32,38 @@ namespace Stars
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            graphics.Clear(Color.Black);
 
+            foreach (var star in stars)
+            {
+                DrawStar(star);
+                MoveStar(star);
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void MoveStar(Star star)
+        {
+            star.Z -= 30;
+
+            if (star.Z < 1)
+            {
+                star.X = random.Next(-pictureBox1.Width, pictureBox1.Width);
+                star.Y = random.Next(-pictureBox1.Height, pictureBox1.Height);
+                star.Z = random.Next(1, pictureBox1.Width);
+            }
+        }
+
+        private void DrawStar(Star star)
+        {
+            float starSize = Map(star.Z, 0, pictureBox1.Width, 7, 0);
+
+            float x = Map(star.X / star.Z, 0, 1, 0, pictureBox1.Width) + pictureBox1.Width / 2;
+
+            float y = Map(star.Y / star.Z, 0, 1, 0, pictureBox1.Height) + pictureBox1.Height / 2;
+
+            graphics.FillEllipse(Brushes.GreenYellow,x,y, starSize,starSize);
         }
 
         private float Map(float n, float start1, float stop1, float start2, float stop2)
@@ -43,7 +74,7 @@ namespace Stars
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            var result = Map(10, 0 , 100, 0, 1000);
+           
 
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
 
@@ -58,7 +89,7 @@ namespace Stars
                     Z = random.Next(1, pictureBox1.Width)
                 };
             }
-
+            timer1.Start();
         }
     }
 }
